3bba956b66d23535b8696b31c9b6a7e8cca3a33d
PNH <phaneendra.hegde@gmail.com>
2013-05-07 06:40:42 +0530

    updated

--- static/js/main.js
+++ static/js/main.js
@@ -8,7 +8,7 @@ $("document").ready(function() {
                 var urlHtml = "<li class=\"url\" id=\"" + data['songs'][i]["id"] + "\" new=\"false\" mod=\"false\"> \
                 <div class=\"url-textarea\" title=\"Open this link\" onclick=\"window.open('"+data['songs'][i]['url']+"')\">" + data["songs"][i]["title"] + "</div> \
                 <div class=\"url-menu\" > Category: <strong>"+ data['songs'][i]['category']+" &nbsp&nbsp&nbsp </strong> submitted by: <strong>"+data["songs"][i]["name"]+"</strong></div> \
-                <br/><img src='"+data['songs'][i]['thumbnail']+"' style='margin-left: 3px; margin-bottom: 2px;'/> <a href='#' class='url-menu'> Find similar </a></li>";
+                <br/><img src='"+data['songs'][i]['thumbnail']+"' style='margin-left: 3px; margin-bottom: 2px;'/> <a href='getComingSoon/' class='url-menu'> Find similar </a></li>";
                 $(".url-list").append(urlHtml);
 
             }
